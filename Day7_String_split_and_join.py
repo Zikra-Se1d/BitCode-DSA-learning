743924c4be8e8eda3934771989a91d5705133dac
Zikra-Se1d <Zikraseiid@gmail.com>
2026-06-17 15:26:53 +0300

    Implement split_and_join function for string manipulation

---
+++ Day7_String_split_and_join.py
@@ -0,0 +1,9 @@
+def split_and_join(line):
+    # write your code here
+    word = line.split(" ")
+    result = "-".join(word)
+    return result
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
